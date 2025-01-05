b8ab7569b4da37611ae1fe0cbc82ed0cc9974e8f
Amer Abdulqeum <alaliamer19@gmail.com>
2025-01-05 22:41:17 +0300

    second push

---
+++ basicreact02/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Youtube from "./Youtube";
+function App() {
+ 
+
+  return (
+    <>
+        <h1>Hello there this a vite/react app !!</h1>
+        <Youtube/>
+    </>
+  )
+}
+
+export default App
---
+++ basicreact02/src/Youtube.jsx
@@ -0,0 +1,7 @@
+function Youtube() {
+    return (
+        <h1>Second File</h1>
+    )
+}
+
+export default Youtube;
