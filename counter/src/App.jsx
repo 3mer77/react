f061d73514bdff2c6f256e884322dd1f1ee784f9
Amer Abdulqeum <alaliamer19@gmail.com>
2025-01-06 18:55:31 +0300

    counterApp

---
+++ counter/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [counter,setcounter] = useState(1);
+
+  // let counter = 0;
+
+  const addcounter = () => {
+
+    // counter += 1
+
+    setcounter((prevcounter) => prevcounter + 1);
+    setcounter((prevcounter) => prevcounter + 1);
+    setcounter((prevcounter) => prevcounter + 1);
+    setcounter((prevcounter) => prevcounter + 1);
+    setcounter((prevcounter) => prevcounter + 1);
+
+  
+  }
+
+  const dec = () => {
+
+    setcounter(counter - 1)
+  }
+
+
+
+
+  return (
+    <>
+      <h1>Hello my name is Amer {counter}</h1>
+      <button onClick={addcounter}>Add Value</button> {"  "}
+      <button onClick={dec}>decrease Value</button>
+      <h2>Footer: {counter}</h2>
+    </>
+  )
+}
+
+export default App
